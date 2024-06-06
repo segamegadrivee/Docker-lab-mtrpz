eaf54cfa05faa0066b94874841de8e4965af41ca
segamegadrivee <sergohack2015@gmail.com>
2024-06-06 14:36:01 +0300

    matrix added

--- lab-03-starter-project-python/spaceship/routers/api.py
+++ lab-03-starter-project-python/spaceship/routers/api.py
@@ -1,8 +1,11 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
-
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@router.get("/matrix")
+def get_matrix():
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    product = np.matmul(matrix_a, matrix_b).tolist()
+    return {"matrix_a": matrix_a, "matrix_b": matrix_b, "product": product}
